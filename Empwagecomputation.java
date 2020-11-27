47e902e4bdd18c421cfedcd5f29427f2773b148a
DineshDan <dineshdanchai341@gmail.com>
2020-11-27 11:46:02 +0530

    Added: Logic to check to compute employee wage for multiple companies

--- Empwagecomputation.java
+++ Empwagecomputation.java
@@ -1,12 +1,9 @@
 public class Empwagecomputation {
 public static final int IS_PART_TIME = 1;
 public static final int IS_FULL_TIME = 2;
-public static final int EMP_RATE_PER_HOUR = 20;
-public static final int NUM_OF_WORKING_DAYS = 2;
-public static final int MAX_HRS_IN_MONTH = 10;
-public static int Empwagecomputation(){
+public static int Empwagecomputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
 int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
-while (totalemphrs <= MAX_HRS_IN_MONTH && totalworkingdays < NUM_OF_WORKING_DAYS)
+while (totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWorkingDays)
 {
 totalworkingdays++;
 int empcheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -23,11 +20,12 @@ emphrs = 0;
 totalemphrs += emphrs;
 System.out.println("Day#: " + totalworkingdays + " Emp Hr: " +emphrs);
 }
-int totalEmpwage  =  totalemphrs * EMP_RATE_PER_HOUR; 
-System.out.println("Total Emp Wage : " + totalEmpwage);
+int totalEmpwage  =  totalemphrs * empRatePerHour; 
+System.out.println("Total Emp Wage for company : " +company+" is: "+totalEmpwage);
 return totalEmpwage;
 }
 public static void main(String args[]) {
-Empwagecomputation();
+Empwagecomputation("HCL", 20, 2, 10);
+Empwagecomputation("Dell", 10, 4, 20);
 }
 } 
