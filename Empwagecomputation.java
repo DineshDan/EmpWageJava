1e8e55589b17b58f6a5b101c2e0d736d89236ac8
DineshDan <dineshdanchai341@gmail.com>
2020-11-28 14:00:30 +0530

    Added: Logic to check ability to save the total wage for each company

--- Empwagecomputation.java
+++ Empwagecomputation.java
@@ -1,12 +1,27 @@
 public class Empwagecomputation {
 public static final int IS_PART_TIME = 1;
 public static final int IS_FULL_TIME = 2;
-public static int Empwagecomputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+
+private final String company ;
+private final int empRatePerHour ;
+private final int numOfWorkingDays ;
+private final int maxHoursPerMonth ;
+private int totalEmpwage;
+public Empwagecomputation(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+super();
+this.company=company;
+this.empRatePerHour=empRatePerHour;
+this.numOfWorkingDays=numOfWorkingDays;
+this.maxHoursPerMonth=maxHoursPerMonth;
+}
+
+public void computeEmpwage()
+{
 int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
 while (totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWorkingDays)
 {
-totalworkingdays++;
 int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+totalworkingdays++;
 switch (empcheck) {
 case IS_PART_TIME:
 emphrs = 4;
@@ -20,12 +35,25 @@ emphrs = 0;
 totalemphrs += emphrs;
 System.out.println("Day#: " + totalworkingdays + " Emp Hr: " +emphrs);
 }
-int totalEmpwage  =  totalemphrs * empRatePerHour; 
-System.out.println("Total Emp Wage for company : " +company+" is: "+totalEmpwage);
-return totalEmpwage;
+ totalEmpwage  =  totalemphrs * empRatePerHour;
 }
-public static void main(String args[]) {
-Empwagecomputation("HCL", 20, 2, 10);
-Empwagecomputation("Dell", 10, 4, 20);
+
+
+
+        public String toString() {
+                return "Total Emp Wage for Company: " +company+ " is: " + totalEmpwage;
+        }
+        public static void main(String[] args) {
+                Empwagecomputation Tesla=new Empwagecomputation("Tesla", 20, 2, 10);
+                Empwagecomputation reliance=new Empwagecomputation("Reliance", 10, 4, 20);
+
+                Tesla.computeEmpwage();
+                System.out.println(Tesla);
+
+
+                reliance.computeEmpwage();
+                System.out.println(reliance);
+
+        }
+
 }
-} 
